74afe41fb0cd2f7b0322b3a4ce19298db63f6813
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-02-20 11:15:53 +0100

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='easy-whisper',
+    name='easy-whisper-local',
     version='1.0',
     packages=find_packages(),
-    requires=open('requirements.txt').readlines(),
+    install_requires=[
+        'openai-whisper'
+    ]
 )
