6eb4ad3ef46b42975a2f8847cd645532dd384b0a
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-02-20 11:44:05 +0100

    Updated description in setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'openai-whisper'
-    ]
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
