038fb93445384eb7e992fdf3e6c32b6fffc9a4d4
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-02-20 17:48:10 +0100

    Streaming added to README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easy-whisper-local',
-    version='1.2.0',
+    version='1.2.5',
     packages=find_packages(),
     install_requires=[
         'openai-whisper',
